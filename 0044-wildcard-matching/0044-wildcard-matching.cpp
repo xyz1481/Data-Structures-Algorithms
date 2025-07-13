d97c7ee46e2406ecccd295323db30c35ec291d7c
Pratik Jadhav <156105845+xyz1481@users.noreply.github.com>
2025-07-13 20:14:51 +0530

    Time: 38 ms (26.35%), Space: 31.5 MB (36.22%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,50 @@
+class Solution {
+private:
+    bool f(int i,int j,string &s,string &p, vector<vector<int>> &dp){
+        if(i<0 && j<0) return true;
+        if(j<0 && i>=0) return false;
+        if(j>=0 && i<0){
+            for(int jj=0;jj<=j;jj++){
+                if(p[jj]!='*') return false;
+            }
+            return true;
+        }
+        if(dp[i][j]!= -1) return dp[i][j];
+        if(s[i]==p[j] || p[j]=='?') return dp[i][j] = f(i-1,j-1,s,p,dp);
+        if(p[j]=='*') return dp[i][j] = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
+        return dp[i][j] = false;
+    }
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return f(n-1,m-1,s,p,dp);
+    }
+};
+
+
+
+// recursion
+// class Solution {
+// private:
+//     bool f(int i,int j,string &s,string &p){
+//         if(i<0 && j<0) return true;
+//         if(i<0 && j>=0) return false;
+//         if(i>=0 && j<0){
+//             for(int ii=0;ii<=i;ii++){
+//                 if(p[ii]!='*') return false;
+//             }
+//             return true;
+//         }
+//         if(s[i]==p[j] || p[j]=='?') return f(i-1,j-1,s,p);
+//         if(p[j]=='*') return f(i-1,j,s,p) || f(i,j-1,s,p);
+//         return false;
+//     }
+// public:
+//     bool isMatch(string s, string p) {
+//         int n = s.size();
+//         int m = p.size();
+//         return f(n-1,m-1,s,p);
+//     }
+// };
